504c7d70f73813841652559f3e2328d578c62293
JTAG <hesamgholami@yahoo.com>
2019-05-12 00:29:39 +0430

    * Add comments for data file reader

--- data-file-reader/build.sbt
+++ data-file-reader/build.sbt
@@ -7,6 +7,6 @@ scalaVersion := "2.12.8"
 libraryDependencies ++= Seq(
   "org.apache.kafka" % "kafka_2.11" % "2.1.1",
   "com.typesafe.akka" %% "akka-stream" % "2.5.21",
-  "com.typesafe.akka" %% "akka-stream-kafka" % "1.0.2",
+  "com.typesafe.akka" %% "akka-stream-kafka" % "1.0.3",
   "io.spray" %%  "spray-json" % "1.3.5"
 )
--- data-file-reader/src/main/scala/Main.scala
+++ data-file-reader/src/main/scala/Main.scala
@@ -1,12 +1,10 @@
 import java.nio.file.Paths
-
 import InsuranceJsonProtocol._
-import akka.Done
 import akka.actor.ActorSystem
 import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.Producer
 import akka.stream.scaladsl._
-import akka.stream.{ActorMaterializer, IOResult, Materializer}
+import akka.stream.{ActorMaterializer, Materializer}
 import akka.util.ByteString
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
@@ -18,18 +16,22 @@ object Main {
 
     println("Hi, This software will read CSV file and send messages to Kafka broker.")
 
+    // CSV sample file path
     println("Opening CSV file ...")
     val file = Paths.get("../data/FL-insurance-sample.csv")
 
+    // Initialize actor system
     println("Creating Akka actor system and materializer ...")
     implicit val system: ActorSystem = ActorSystem("insuranceProducerSystem")
     implicit val materializer: Materializer = ActorMaterializer()
 
+    // Kafka options
     val bootstrapServers = "localhost:9094"
     val topic = "insurance-fl"
 
     println("Initializing Kafka producer ...")
 
+    // Kafka producer configurations
     val config = system.settings.config.getConfig("akka.kafka.producer")
     val producerSettings =
       ProducerSettings(config, new StringSerializer, new StringSerializer)
@@ -38,16 +40,21 @@ object Main {
     println("Sending data to Kafka broker ...")
 
     val done =
+      // Read from CSV file as our source
       FileIO.fromPath(file)
+        // Separate each line by standard line terminator to be processed individually by flow
         .via(Framing.delimiter(ByteString("\n"), 256, allowTruncation = true))
         .drop(1) // First line is CSV headers, so we drop it
         .map { line =>
+        // Convert line to JSON string and send it to Kafka broker
         val cols = line.utf8String.split(",").map(_.trim)
         val insuranceItem = InsuranceItem(cols(0).toInt, cols(2), cols(3).toDouble, cols(15), Point(cols(13).toDouble, cols(14).toDouble))
         println("Sending item to kafka broker: " + insuranceItem)
         new ProducerRecord[String, String](topic, insuranceItem.policyID.toString, insuranceItem.toJson.compactPrint)
         }
+        // Enable error logging in console
         .log("error logging")
+        // Send stream to Kafka producer sink
         .runWith(Producer.plainSink(producerSettings))
 
   }
